2d76f36f23f277ef9f6681826620deb0c2326608
elleaech <lohan.chuan123@gmail.com>
2022-05-11 23:46:29 -0300

    Removed ota logic from file_server

--- main/file_server.c
+++ main/file_server.c
@@ -290,16 +290,6 @@ static esp_err_t download_get_handler(httpd_req_t *req)
     return ESP_OK;
 }
 
-static void do_ota(void)
-{
-    HTTP http_data = {
-        .url = "http://192.168.1.14/lb_wifi.bin",
-    };
-
-    int rc = lb_ota_update_firmware_perform(&http_data, NULL, NULL);
-    lb_ota_update_firmware_finish(rc);
-}
-
 /* Handler to upload a file onto the server */
 static esp_err_t upload_post_handler(httpd_req_t *req)
 {
@@ -411,8 +401,6 @@ static esp_err_t upload_post_handler(httpd_req_t *req)
     httpd_resp_set_hdr(req, "Connection", "close");
 #endif
     httpd_resp_sendstr(req, "File uploaded successfully");
-
-    do_ota();
 
     return ESP_OK;
 }
